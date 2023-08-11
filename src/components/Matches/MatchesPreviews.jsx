65493c7026860f60943d97e28b78c0846bf95bf4
glowcloud <oliwia.rogala97@gmail.com>
2023-08-11 15:52:39 +0200

    Matches component split

---
+++ src/components/Matches/MatchesPreviews.jsx
@@ -0,0 +1,72 @@
+import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import CharacterSelect from "./CharacterSelect";
+import CharacterRow from "./CharacterRow";
+import CustomPagination from "../CustomPagination";
+import { paginate } from "../../utils/paginate";
+
+const MatchesPreviews = ({
+  matches,
+  side,
+  characterChoice,
+  setCharacterChoice,
+  currentPage,
+  setCurrentPage,
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <Box>
+      {/* CHARACTER FILTER */}
+      <CharacterSelect
+        characterChoice={characterChoice}
+        setCharacterChoice={setCharacterChoice}
+        matches={matches}
+        side={side}
+      />
+
+      {/* MATCHES */}
+      {paginate(
+        matches.filter(
+          (match) =>
+            match.side === side &&
+            (characterChoice === "all" || match.character.id == characterChoice)
+        ),
+        5,
+        currentPage
+      ).map((match, index) => (
+        <Box
+          key={index}
+          sx={{
+            mx: 5,
+            "&:hover": {
+              cursor: "pointer",
+            },
+          }}
+          onClick={() => navigate(`/matches/${match.id}`)}
+        >
+          <CharacterRow data={match} player={false} side={match.side} />
+        </Box>
+      ))}
+
+      {/* PAGINATION */}
+      {side && matches.length > 0 && (
+        <CustomPagination
+          itemsCount={
+            matches.filter(
+              (match) =>
+                match.side === side &&
+                (characterChoice === "all" ||
+                  match.character.id == characterChoice)
+            ).length
+          }
+          itemsPerPage={5}
+          page={currentPage}
+          setPage={setCurrentPage}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default MatchesPreviews;
--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,14 +1,10 @@
 import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { paginate } from "../utils/paginate";
-import CustomPagination from "../components/CustomPagination";
-import CharacterRow from "../components/Matches/CharacterRow";
 import StatsCharts from "../components/Matches/StatsCharts";
-import { fetchMatches } from "../utils/getMatchUtils";
 import SideChoice from "../components/SideChoice";
 import ViewButtons from "../components/Matches/ViewButtons";
-import CharacterSelect from "../components/Matches/CharacterSelect";
+import MatchesPreviews from "../components/Matches/MatchesPreviews";
+import { fetchMatches } from "../utils/getMatchUtils";
 
 const Matches = () => {
   const [loading, setLoading] = useState(true);
@@ -17,7 +13,6 @@ const Matches = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [characterChoice, setCharacterChoice] = useState("all");
   const [showStats, setShowStats] = useState(false);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const getMatches = async () => {
@@ -64,57 +59,14 @@ const Matches = () => {
 
           {/* MATCHES PREVIEW */}
           {!showStats && matches?.length > 0 && (
-            <Box>
-              {/* CHARACTER FILTER */}
-              <CharacterSelect
-                characterChoice={characterChoice}
-                setCharacterChoice={setCharacterChoice}
-                matches={matches}
-                side={side}
-              />
-
-              {/* MATCHES */}
-              {paginate(
-                matches.filter(
-                  (match) =>
-                    match.side === side &&
-                    (characterChoice === "all" ||
-                      match.character.id == characterChoice)
-                ),
-                5,
-                currentPage
-              ).map((match, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    mx: 5,
-                    "&:hover": {
-                      cursor: "pointer",
-                    },
-                  }}
-                  onClick={() => navigate(`/matches/${match.id}`)}
-                >
-                  <CharacterRow data={match} player={false} side={match.side} />
-                </Box>
-              ))}
-
-              {/* PAGINATION */}
-              {side && matches.length > 0 && (
-                <CustomPagination
-                  itemsCount={
-                    matches.filter(
-                      (match) =>
-                        match.side === side &&
-                        (characterChoice === "all" ||
-                          match.character.id == characterChoice)
-                    ).length
-                  }
-                  itemsPerPage={5}
-                  page={currentPage}
-                  setPage={setCurrentPage}
-                />
-              )}
-            </Box>
+            <MatchesPreviews
+              matches={matches}
+              side={side}
+              characterChoice={characterChoice}
+              setCharacterChoice={setCharacterChoice}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
           )}
         </Box>
       )}
